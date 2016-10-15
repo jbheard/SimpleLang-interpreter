2954c7bc3c37ac6255b53987ee72095b18471237
Jacob Heard <heard.jacob.7@gmail.com>
2016-10-15 11:24:45 -0400

    Firt Commit

---
+++ brainfuck.c
@@ -0,0 +1,164 @@
+/** Interactive Brainfuck Interpreter
+ * @author Jacob Heard
+ * Date: October 11th, 2016 
+ * 
+ * Description: A command line Brainfuck interpreter, runs brainfuck code from a file or from the command line.
+ * Brainfuck is an esoteric programming language in which there are only 8 operations, these are as follows:
+ * 		< 	Decrements the data pointer
+ * 		> 	Increments the data pointer
+ * 		- 	Decrements the byte at the data pointer
+ * 		+ 	Increments the byte at the data pointer
+ * 		[	Begins a loop, if byte at data pointer is 0, skip to the command after end of loop
+ * 		] 	Jump to beginning of loop
+ *		,	Take a single byte of input
+ * 		. 	Print a the byte at the data pointer 
+ *  
+ * Anything that is not one of the above 8 operations is ignored, and can be used to comment 
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "brainfuck.h"
+
+
+int main(int argc, char* argv[])
+{
+	FILE * fp; // File pointer
+	if(argc > 1)
+	{
+		if(strcmp(argv[1], "--help") == 0)
+		{ // If the user is begging for help
+			printf("Usage: %s <filename>\n", argv[0]);
+			return 1;
+		}
+		else // Open the given file
+			fp = fopen(argv[1], "r");
+	}
+	else
+	{
+		// Otherwise, run in interactive mode, accept brainfuck from the command line
+		fp = stdin;
+	}
+
+	int where = 0, len; // memory location, length of input
+	
+	char *raw; // Buffer for unprocessed input
+	char *buf; // Buffer for processed input
+	char memory[BF_ARRAY_SIZE] = {0}; // The Brainfuck program space
+
+	if(argc == 1)
+	{
+		raw = malloc(NUM_BYTES);
+	}
+	else
+	{
+		long filelen;
+		
+		fp = fopen(argv[1], "rb");  // Open the file in binary mode
+		if(fp == NULL)
+		{
+			printf("Error: file not found.\n");
+			return 1;
+		}
+		fseek(fp, 0, SEEK_END);          // Jump to the end of the file
+		filelen = ftell(fp);             // Get the current byte offset in the file
+		rewind(fp);                      // Jump back to the beginning of the file
+		
+		raw = malloc((filelen+1)); // Enough memory for file + \0
+		fread(raw, filelen, 1, fp); // Read in the entire file
+		fclose(fp); // Close the file;
+	}
+	
+	do
+	{
+		if(argc == 1) // Command line mode	
+		{
+			printf(": ");
+			memset(raw, 0, NUM_BYTES); // Zero the buffer
+			fgets(raw, NUM_BYTES, fp); // Read from input
+			
+			// If the user wants to quit
+			if(strncmp(raw, "quit", 4) == 0)
+			{
+				free(raw);
+				return 1;
+			}
+			// If the user wants to start over
+			else if(strncmp(raw, "reset", 5) == 0)
+			{
+				// Reset variables
+				where = 0;
+				memset(memory, 0, BF_ARRAY_SIZE);
+				continue;
+			}
+		}
+		
+		len = parse(raw, &buf); // Process raw input, get parse length
+		if(len < 0)
+			continue;
+
+		if(strncmp(raw, "debug", 5) == 0)
+		{
+			printf("Where: %d\n", where);
+			for(int i = 0; i < 8; i++)
+				printf("%d:%c | ", (int)memory[i], memory[i]);
+			printf("\n");
+		}
+		
+		// Excecute the Brainfuck code
+		for(int i = 0; i < len; i++)
+		{
+			switch(buf[i])
+			{
+				case '+': 
+					++ memory[where];
+					break;
+				case '-':
+					-- memory[where];
+					break;
+				case '<':
+					-- where;
+					break;
+				case '>':
+					++ where;
+					break;
+				case '.':
+					putchar(memory[where]);
+					break;
+				case ',':
+					memory[where] = (char)getchar();
+					break;
+				case '[':
+					if(memory[where] == 0)
+						i += (int)*((short*)&buf[i+1])-1;
+					else
+						i += sizeof(short);
+					break;
+				case ']':
+					i += (int)*((short*)&buf[i+1])-1;
+					break;
+			}
+			
+			// Handle errors
+			if(where < 0 || where > BF_ARRAY_SIZE)
+			{
+				printf("Runtime error at operation %d; %c\n", i, buf[i]);
+				if(argc == 1)
+				{
+					printf("Resetting brainfuck...\n");
+					where = 0;
+					memset(memory, 0, BF_ARRAY_SIZE);
+				}
+				break;
+			}
+		} // End for
+		printf("\n");
+		free(buf);
+	} while( argc == 1 ); // End while	
+	
+	free(raw);
+	
+	return 0;
+}
---
+++ brainfuck.h
@@ -0,0 +1,116 @@
+#ifndef BRAINFUCK_H
+#define BRAINFUCK_H	
+
+#define BF_ARRAY_SIZE 65536 // 2^16 bytes usable data space
+#define NUM_BYTES	  1024  // Read 4096 bytes at a time
+
+/* Finds the length of a negative-terminated short array
+ * @param arr The short array
+ * @return The number of shorts before the first negative in the array (int)
+ */
+int shortlen(short *arr)
+{
+	short *ptr = arr, len = 0;
+	while(*ptr++ >= 0) ++len;
+	return len;
+}
+
+/* Pushes a short onto an array
+ * @param stack The array pointer
+ * @param data The into to push
+ */
+void push(short *stack, short data)
+{
+	short *tmp = &stack[shortlen(stack)];
+	*tmp = data;
+}
+
+/* Removes and returns the last element in a short array
+ * @param stack The array pointer
+ * @return The last element of stack (short)
+ */
+short pop(short *stack)
+{
+	int len = shortlen(stack);
+	if(len == 0)
+		return -1;
+	short tmp = stack[len-1];
+	stack[len-1] = -1;
+	return tmp;
+}
+
+/* Returns the last element in an int array without removing it
+ * @param stack The array pointer
+ * @return The last element of stack (short)
+ */
+short peek(short *stack)
+{
+	int len = shortlen(stack);
+	if(len == 0)
+		return -1;
+	return stack[len-1];
+}
+
+/* Parses Brainfuck code into more easily-executed operations
+ * @param bf The brainfuck code to parse
+ * @param arr Pointer to the location to store the modified code
+ * @return The length of the newly parsed array
+ */
+int parse(char *bf, char **arr)
+{
+	int cnt = 0;
+	for(int i = 0; i < strlen(bf); i++)
+	{ // Counts the number of actual instructions
+		switch(bf[i])
+		{
+			case '<': case '>': case '+':
+			case '-': case ',': case '.':
+				cnt += 1; // Standard operation
+				break;
+			case '[': case ']':
+				cnt += 1 + sizeof(short); // Loop ops require a memory address
+				break;
+		}
+	}
+
+	char *ptr = malloc(cnt); // malloc for formatted code
+	*arr = ptr;	
+	memset(*arr, 0, cnt);
+	
+	cnt = 0; // reset cnt for loop
+	short tmp, loopstack[64]; // Stores loop pointers
+
+	for(int i = 0; i < 64; i++)
+		loopstack[i] = -1;
+	
+	for(int i = 0; i < strlen(bf); i++)
+	{
+		switch(bf[i])
+		{
+			case '<': case '>': case '+':
+			case '-': case ',': case '.':
+				// Store the operation
+				ptr[cnt] = bf[i];
+				cnt += 1;
+				break;
+			case '[':
+				// Store the operation, push loop address
+				ptr[cnt] = bf[i];
+				push(loopstack, cnt);
+				cnt += 1 + sizeof(short); // 1 byte for operator, 2 bytes for address
+				break;
+			case ']':
+				// Store the operation, pop loop address, set address variables
+				ptr[cnt] = bf[i];
+				tmp = pop(loopstack);
+				*((short*)&ptr[cnt+1]) = tmp-cnt;
+				cnt += 1 + sizeof(short);
+				*((short*)&ptr[tmp+1]) = cnt-tmp;
+				break;
+		} // End switch
+	} // End for
+
+	return cnt;
+}
+
+#endif // BRAINFUCK_H
