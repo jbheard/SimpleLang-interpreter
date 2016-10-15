766761328e21831b61ef7e67e7d419dd0f5acd86
Jacob Heard <heard.jacob.7@gmail.com>
2016-10-15 18:37:41 -0400

    Added brainfuck++ interpreter

---
+++ brainfuckpp.c
@@ -0,0 +1,253 @@
+/** Interactive Brainfuck++ Interpreter
+ * @author Jacob Heard
+ * Date: October 15th, 2016 
+ * 
+ * Description: A command line brainfuck++ interpreter, runs brainfuck++ code from a file or from the command line.
+ * Brainfuck is an esoteric programming language in which there are only 8 operations, these are as follows:
+ * 		< 	Decrements the data pointer
+ * 		> 	Increments the data pointer
+ * 		- 	Decrements the byte at the data pointer
+ * 		+ 	Increments the byte at the data pointer
+ * 		[	Begins a loop, if byte at data pointer is 0, skip to the command after end of loop
+ * 		] 	Jump to beginning of loop
+ *		,	Take a single byte of input
+ * 		. 	Print a the byte at the data pointer 
+ * 
+ * Brainfuck++ is an edition to the brainfuck language outlined by Jacob I. Torrey (https://esolangs.org/wiki/Brainfuck%2B%2B).
+ * I stumbled across the language browsing through Esolang and it caught my eye. I personally think that brainfuck is great as
+ * is, but I was still disapointed to see that nobody has written a proper implementation of brainfuck ++. At least to my 
+ * current knowledge, there are only a few interpreters out there for this, and none of them actually obey the specifications,
+ * all of which simply add many extra commands that the developer thought would be neat. Even the creator of the specification 
+ * did not complete anything more than a simple brainfuck interpreter (http://www.jitunleashed.com/bf/index.html).
+ * 
+ * The added operations in the brainfuck++ language are:
+ *		#	Open a file for reading/writing
+ *		; 	write the character in the current cell to the file, overwriting what is in the file
+ * 		: 	Read a character from the file
+ *		% 	Opens a socket for reading/writing. A second call closes the socket.
+ *		^ 	Sends the character in the current cell
+ * 		!	Reads a character from socket into current cell
+ *
+ * As per brainfuck standard, anything that is not one of the above 14 operations is ignored, and can be used to comment 
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "brainfuckpp.h"
+
+
+int main(int argc, char* argv[])
+{
+	FILE * fp; // File pointer
+	if(argc > 1)
+	{
+		if(strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0)
+		{ // If the user is begging for help
+			printf("Usage: %s <filename>\n", argv[0]);
+			return 1;
+		}
+		else // Open the given file
+			fp = fopen(argv[1], "r");
+	}
+	else
+	{
+		// Otherwise, run in interactive mode, accept brainfuck from the command line
+		fp = stdin;
+	}
+
+	int where = 0, roll_where = 0, len; // memory location, prev. memory location, length of input
+	char file_open = 0, sock_open = 0; // Whether we have sockets or files open
+	FILE* bf_fp = NULL; // Brainfuck file pointer
+	
+	SOCKET sock; // Socket for working with servers
+	int port;	 // Port number for working with servers
+	
+	char *raw; // Buffer for unprocessed input
+	char *buf; // Buffer for processed input
+	char memory[BF_ARRAY_SIZE] = {0}; // The Brainfuck program space
+	char rollback[BF_ARRAY_SIZE] = {0}; // Copy of memory in case of error
+
+	if(argc == 1) // Command line mode
+	{
+		raw = malloc(NUM_BYTES);
+	}
+	else
+	{
+		long filelen; // length of the file
+		
+		fp = fopen(argv[1], "rb");  // Open the file in binary mode
+		if(fp == NULL)
+		{
+			printf("Error: file not found.\n"); // Uh-oh spaghettios
+			return 1;
+		}
+		fseek(fp, 0, SEEK_END);          // Jump to the end of the file
+		filelen = ftell(fp);             // Get the current byte offset in the file
+		rewind(fp);                      // Jump back to the beginning of the file
+		
+		raw = malloc((filelen+1)); // Enough memory for file + '\0'
+		fread(raw, filelen, 1, fp); // Read in the entire file
+		fclose(fp); // Close the file;
+	}
+	
+	do
+	{
+		if(argc == 1) // Command line mode	
+		{
+			printf(": ");			   // Make it look pretty
+			memset(raw, 0, NUM_BYTES); // Zero the buffer
+			fgets(raw, NUM_BYTES, fp); // Read from input
+			
+			// If the user wants to quit
+			if(strncmp(raw, "quit", 4) == 0)
+			{
+				free(raw);
+				return 1;
+			}
+			// If the user wants to start over
+			else if(strncmp(raw, "reset", 5) == 0)
+			{
+				// Reset variables
+				where = 0;
+				memset(memory, 0, BF_ARRAY_SIZE);
+				continue;
+			}
+			else if(strncmp(raw, "where", 5) == 0)
+				printf("Cell %d -> contains %c - %d\n", where, memory[where], (int)memory[where]);
+		}
+		
+		len = parse(raw, &buf); // Process raw input, get parse length
+		if(len < 0)
+			continue;
+		
+		// Excecute the Brainfuck code
+		for(int i = 0; i < len; i++)
+		{
+			switch(buf[i])
+			{
+				case '+': // Increment byte
+					++ memory[where];
+					break;
+				case '-': // Decrement byte
+					-- memory[where];
+					break;
+				case '<': // Move pointer -
+					-- where;
+					break;
+				case '>': // Move pointer +
+					++ where;
+					break;
+				case '.': // Write byte
+					putchar(memory[where]);
+					break;
+				case ',': // Read byte
+					memory[where] = (char)getchar();
+					break;
+				case '[': // Start loop
+					if(memory[where] == 0)
+						i += (int)*((short*)&buf[i+1])-1;
+					else
+						i += sizeof(short);
+					break;
+				case ']': // End loop
+					i += (int)*((short*)&buf[i+1])-1;
+					break;
+				case '#': // Open file
+					if(file_open && bf_fp != NULL)
+					{
+						fclose(bf_fp);
+						file_open = 0;
+					}
+					else
+					{
+						bf_fp = fopen(&memory[where+1], "rb+");
+						if(bf_fp == NULL) // Failure :(
+							memory[where] = -1;
+						else 			  // Success :)
+							memory[where] = 0;
+						file_open = 1; // File is now open
+					}
+					break;
+				case ';': // Write byte to file
+					if(bf_fp == NULL)
+						where = -2;
+					else
+						putc(memory[where], bf_fp);
+					break;
+				case ':': // Read byte from file
+					if(bf_fp == NULL)
+						where = -2;
+					else
+						memory[where] = (char)getc(bf_fp);
+					break;
+				case '%':
+					if(sock_open)
+					{
+						close_sock(sock);
+						sock_open = 0;
+					}
+					else
+					{
+					/* I hate writing compicated code, because now I have to explain it  
+					* for future me and anyone who might see this.
+					* 
+					* First off, the memory layout when opening a port should be:
+					* 		[1 byte return value] [n byte URL/IP] [1 NULL byte] [2 byte port number]
+					*
+					* 1. The first line here uses port as a temporary variable to store the length
+					* 		of the URL/IP. 
+					* 2. The second line uses this to access the memory where the port 
+					* 		number is. It is always in Big Endian format, so the first byte is 
+					*		multiplied by 256 and added to the second byte.
+					* 3. The third line opens the port and sets the return value.
+					*/
+						port = strlen(&memory[where+1]);
+						port = memory[where+1+port]*0x100 + memory[where+2+port];
+						memory[where] = open_sock(&sock, &memory[where+1], port);
+						sock_open = 1;
+					}
+					break;
+				case '^': // Send 1 byte through socket
+					if(sock_open)
+						send_sock(sock, memory[where]);
+					break;
+				case '!': // Recv 1 byte through socket
+					if(sock_open)
+						memory[where] = recv_sock(sock);
+					break;
+			}
+			
+			// Handle errors
+			if(where < 0 || where >= BF_ARRAY_SIZE)
+			{
+				printf("Runtime error at operation %d; %c : %s\n", i, buf[i], get_error(where));
+				if(argc == 1)
+				{
+					printf("Rolling back brainfuck memory...\n");
+					strncpy(memory, rollback, BF_ARRAY_SIZE);
+					where = roll_where;
+					memset(memory, 0, BF_ARRAY_SIZE);
+				}
+				break;
+			}
+		} // End for
+		
+		printf("\n"); // Makes output look prettier
+		free(buf); // Free this so we can use it again
+		
+		// Save the current data, if something goes wrong next time we can roll back
+		roll_where = where;
+		strncpy(rollback, memory, BF_ARRAY_SIZE);
+	} while( argc == 1 ); // End while
+	
+	// Clean up after the user
+	if(file_open) 
+		fclose(bf_fp);
+	if(sock_open)
+		close_sock(sock);
+	
+	free(raw);
+	return 0;
+}
---
+++ brainfuckpp.h
@@ -0,0 +1,254 @@
+#ifndef BRAINFUCK_H
+#define BRAINFUCK_H	
+
+// Stupid windows with their stupid winsock, making me do extra work
+#ifdef __WIN32__
+	#include <windows.h>
+	#include <winsock.h>
+#else
+	#include <sys/socket.h>
+	#include <arpa/inet.h> //inet_addr
+
+	#define INVALID_SOCKET -1
+	typedef SOCKET int;
+#endif
+
+#define BF_ARRAY_SIZE 32768 // 2^15 bytes usable data space
+#define NUM_BYTES	  1024  // Read up to 1024 bytes at a time in the console
+
+
+/* Opens a socket and connects to a given host on a given port
+ * NOTE: Winsock code is from http://johnnie.jerrata.com/winsocktutorial/ 
+ *
+ * @param s The socket to open
+ * @param hostname The URL or IP of the host to connect to
+ * @param portno The port to use when we connect
+ * @return 0 if everything went well, -1 otherwise (char)
+ */
+char open_sock(SOCKET* s, char* hostname, int portno)
+{
+#ifdef __WIN32__
+	WORD sockVersion;
+	WSADATA wsaData;
+	
+	sockVersion = MAKEWORD(1, 1);
+	
+	// Initialize Winsock
+	WSAStartup(sockVersion, &wsaData);
+#endif
+	
+	// Store information about the server
+	LPHOSTENT hostEntry;
+	
+	// Specifying the server by its name;
+	if (!(hostEntry = gethostbyname(hostname)))
+	{
+#ifdef __WIN32__
+		WSACleanup();
+#endif
+		return -1;
+	}
+
+	// Create the socket
+	*s = socket(AF_INET,			// Go over TCP/IP
+			   SOCK_STREAM,			// This is a stream-oriented socket
+			   IPPROTO_TCP);		// Use TCP rather than UDP
+
+	if (*s == INVALID_SOCKET)
+	{
+#ifdef __WIN32__
+		WSACleanup();
+#endif
+		return -1;
+	}
+	
+	// Fill a SOCKADDR_IN struct with address information
+	SOCKADDR_IN serverInfo;
+	serverInfo.sin_family = AF_INET;
+
+	// Change to network-byte order and
+	// insert into port field
+	serverInfo.sin_addr = *((LPIN_ADDR)*hostEntry->h_addr_list);
+	serverInfo.sin_port = htons(portno);
+
+	if(connect(*s, (LPSOCKADDR)&serverInfo, sizeof(struct sockaddr)) == SOCKET_ERROR)
+	{
+#ifdef __WIN32__
+		WSACleanup();
+#endif
+		return -1;
+	}
+	
+	return 0;
+}
+
+void close_sock(SOCKET s)
+{
+#ifdef __WIN32__
+	closesocket(s);
+	WSACleanup();
+#else
+	close(s);
+#endif
+}
+
+/* Sends a single byte from an open socket
+ * @param s The socket to write to 
+ * @param byte The data to send
+ */
+void send_sock(SOCKET s, char byte)
+{
+	send(s, &byte, 1, 0);
+}
+
+/* Recieves and returns a single byte from an open socket
+ * @param s The socket to read from
+ * @return The byte recieved from the socket (char)
+ */
+char recv_sock(SOCKET s)
+{
+	char byte;
+	recv(s, &byte, 1, 0);
+	return byte;
+}
+
+/* Finds the length of a negative-terminated short array
+ * @param arr The short array
+ * @return The number of shorts before the first negative in the array (int)
+ */
+int shortlen(short *arr)
+{
+	short *ptr = arr, len = 0;
+	while(*ptr++ >= 0) ++len;
+	return len;
+}
+
+/* Pushes a short onto an array
+ * @param stack The array pointer
+ * @param data The into to push
+ */
+void push(short *stack, short data)
+{
+	short *tmp = &stack[shortlen(stack)];
+	*tmp = data;
+}
+
+/* Removes and returns the last element in a short array
+ * @param stack The array pointer
+ * @return The last element of stack (short)
+ */
+short pop(short *stack)
+{
+	int len = shortlen(stack);
+	if(len == 0)
+		return -1;
+	short tmp = stack[len-1];
+	stack[len-1] = -1;
+	return tmp;
+}
+
+/* Returns the last element in an int array without removing it
+ * @param stack The array pointer
+ * @return The last element of stack (short)
+ */
+short peek(short *stack)
+{
+	int len = shortlen(stack);
+	if(len == 0)
+		return -1;
+	return stack[len-1];
+}
+
+/* Parses Brainfuck code into more easily-executed operations
+ * @param bf The brainfuck code to parse
+ * @param arr Pointer to the location to store the modified code
+ * @return The length of the newly parsed array
+ */
+int parse(char *bf, char **arr)
+{
+	int cnt = 0;
+	for(int i = 0; i < strlen(bf); i++)
+	{ // Counts the number of actual instructions
+		switch(bf[i])
+		{
+			case '<': case '>': case '+':
+			case '-': case ',': case '.':
+			case '#': case '^': case '!':
+			case ';': case ':': case '%':
+				cnt += 1; // Standard operation
+				break;
+			case '[': case ']':
+				cnt += 1 + sizeof(short); // Loop ops require a 2 byte relative address
+				break;
+		}
+	}
+
+	char *ptr = malloc(cnt); // malloc for formatted code
+	*arr = ptr;	
+	memset(*arr, 0, cnt);
+	
+	cnt = 0; // reset cnt for loop
+	short tmp, loopstack[64]; // Stores loop pointers
+
+	for(int i = 0; i < 64; i++)
+		loopstack[i] = -1; // Init all loop locations to -1
+	
+	for(int i = 0; i < strlen(bf); i++)
+	{
+		switch(bf[i])
+		{
+			case '<': case '>': case '+':
+			case '-': case ',': case '.':
+			case '#': case '^': case '!':
+			case ';': case ':': case '%':
+				// Store the operation
+				ptr[cnt] = bf[i];
+				cnt += 1;
+				break;
+			case '[':
+				// Store the operation, push loop address
+				ptr[cnt] = bf[i];
+				push(loopstack, cnt);
+				cnt += 1 + sizeof(short); // 1 byte for operator, 2 bytes for address
+				break;
+			case ']':
+				// Store the operation, pop loop address, set address variables
+				
+				/* This code is a stupid awful hack, but I love it.
+				* Basically, we keep 2 bytes after every loop op ('[' and ']'),
+				* we store an integer there, the relative position of the other
+				* end of the loop. This means we can quickly jump from the end to the 
+				* beginning or vice versa.
+				*/
+				ptr[cnt] = bf[i];
+				tmp = pop(loopstack);
+				*((short*)&ptr[cnt+1]) = tmp-cnt;
+				cnt += 1 + sizeof(short);
+				*((short*)&ptr[tmp+1]) = cnt-tmp;
+				break;
+		} // End switch
+	} // End for
+
+	return cnt;
+}
+
+/* Gets an error message corresponding to an error code
+ * @param err The error code
+ * @return The error message corresponding to the error code (const char*)
+ */
+const char* get_error(int err)
+{
+	switch(err)
+	{
+		case -1:
+			return "Pointer index out of bounds.";
+		case -2: 
+			return "File not open for reading.";
+		case BF_ARRAY_SIZE:
+			return "Maximum buffer size surpassed.";
+		default:
+			return "Unknown error.";
+	}
+}
+
+#endif // BRAINFUCK_H
